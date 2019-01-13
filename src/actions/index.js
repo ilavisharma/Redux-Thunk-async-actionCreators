56211c989e9f4c830dd775a226c832f12e5ff9f3
Lavi <lavisharma796@gmail.com>
2019-01-13 09:44:54 +0000

    update reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,36 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPostsAndUsers = () => async(dispatch, getState) => {
+    console.log('about to fetch posts');
+    await dispatch(fetchPosts()); // we wait for this api request to complete
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
 
 export const fetchPosts = () => async(dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = (id) => async(dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// --- MEMOIZED VERSION ---
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async(id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
-
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
         return this.props.posts.map(post => {
-            return (
-                <div className="item" key={post.id}>
+            return (<div className="item" key={post.id}>
                     <i className="large middle aligned icon user" />
                     <div className="content">
                         <div className="description">
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId} />
                     </div>
-                </div>
-            );
+                </div>);
         });
     }
 
     render() {
-        return (
-            <div className="ui relaxed divided list">
+        return (<div className="ui relaxed divided list">
                 {this.renderList()}
-            </div>
-        );
+            </div>);
     }
 }
 
@@ -39,4 +36,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers: fetchPostsAndUsers })(PostList);
